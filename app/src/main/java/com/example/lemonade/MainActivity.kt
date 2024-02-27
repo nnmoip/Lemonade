32d2625712ea6662f27f3fcd86043def92d8932f
Noellie Paulus <noelliepaulus@etu.univ-lyon1.fr>
2024-02-27 18:19:22 +0100

    Organisation des pages avec les différents clicks

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -3,30 +3,24 @@ package com.example.lemonade
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -58,22 +52,58 @@ fun LemonadeApp() {
 
 @Composable
 fun LemonadeProducer(modifier: Modifier = Modifier) {
-    LemonadePage(
-        painterImage = painterResource(id = R.drawable.lemon_tree),
-        imageDesc = stringResource(R.string.lemon_tree),
-        instructions = stringResource(R.string.instructions_1),
-        modifier = modifier
-    )
+    var pageToLoad by remember { mutableIntStateOf(1) }
+    var toPress by remember { mutableIntStateOf(4) }
+    when (pageToLoad) {
+        1 -> LemonadePage(
+                painterImage = painterResource(id = R.drawable.lemon_tree),
+                imageDesc = stringResource(R.string.lemon_tree),
+                instructions = stringResource(R.string.instructions_1),
+                modifier = modifier,
+                imageOnClick = {
+                    pageToLoad = 2
+                    toPress = (2..4).random()
+                }
+            )
+        2 -> LemonadePage(
+                painterImage = painterResource(id = R.drawable.lemon_squeeze),
+                imageDesc = stringResource(R.string.lemon),
+                instructions = stringResource(R.string.instructions_2),
+                modifier = modifier,
+                imageOnClick = {
+                    if(toPress == 0) pageToLoad = 3
+                    else toPress--
+                }
+            )
+        3 -> LemonadePage(
+                painterImage = painterResource(id = R.drawable.lemon_drink),
+                imageDesc = stringResource(R.string.lemonade_glass),
+                instructions = stringResource(R.string.instructions_3),
+                modifier = modifier,
+                imageOnClick = {
+                    pageToLoad = 4
+                }
+            )
+        else -> LemonadePage(
+                    painterImage = painterResource(id = R.drawable.lemon_restart),
+                    imageDesc = stringResource(R.string.empty_glass),
+                    instructions = stringResource(R.string.instructions_4),
+                    modifier = modifier,
+                    imageOnClick = {
+                        pageToLoad = 1
+                    }
+                )
+    }
 }
 
 @Composable
-fun LemonadePage(painterImage: Painter, imageDesc: String, instructions: String, modifier: Modifier = Modifier) {
+fun LemonadePage(painterImage: Painter, imageDesc: String, instructions: String, imageOnClick: () -> Unit, modifier: Modifier = Modifier) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier
     ) {
         Button(
-            onClick = { /*TODO*/ },
+            onClick = imageOnClick,
             shape = RoundedCornerShape(32.dp),
             colors = ButtonDefaults.outlinedButtonColors(Color(0xFFB1F3D4))
         ) {
