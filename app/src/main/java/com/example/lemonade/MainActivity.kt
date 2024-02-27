622530511af3722e849448e53ea42dc2ee9a1434
Noellie Paulus <noelliepaulus@etu.univ-lyon1.fr>
2024-02-27 17:30:19 +0100

    Init interface première page

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -3,13 +3,34 @@ package com.example.lemonade
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.lemonade.ui.theme.LemonadeTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,11 +44,44 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun LemonadeApp() {
+    LemonadeProducer(
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
+    )
 }
 
 @Composable
 fun LemonadeProducer(modifier: Modifier = Modifier) {
+    LemonadePage(
+        painterImage = painterResource(id = R.drawable.lemon_tree),
+        imageDesc = stringResource(R.string.lemon_tree),
+        instructions = stringResource(R.string.instructions_1),
+        modifier = modifier
+    )
+}
+
+@Composable
+fun LemonadePage(painterImage: Painter, imageDesc: String, instructions: String, modifier: Modifier = Modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        Button(
+            onClick = { /*TODO*/ },
+        ) {
+            Image(
+                painter = painterImage,
+                contentDescription = imageDesc
+            )
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = instructions,
+            fontSize = 18.sp
+        )
+    }
 }
